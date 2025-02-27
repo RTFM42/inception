a76500d631d048ee4f381c31c65a36d7f5901e74
yush <yus-sato@kalyte.ro>
2025-02-27 06:10:22 +0000

    update

--- srcs/requirements/php/docker-entrypoint.sh
+++ srcs/requirements/php/docker-entrypoint.sh
@@ -4,8 +4,14 @@ set -e
 
 if [ ! -d /var/www/html ]; then
     mkdir -p /var/www/html
-    chown www-data:www-data /var/www/html
+    chown -R www-data:www-data /var/www/html
     chmod 1777 /var/www/html
+fi
+if [ ! -d /var/www/html/wordpress ]; then
+    curl -sLO https://ja.wordpress.org/latest-ja.zip
+    unzip -d /var/www/html latest-ja.zip
+    chown -R www-data:www-data /var/www/html/wordpress
+    chmod 1777 /var/www/html/wordpress
 fi
 
 exec "$@"
